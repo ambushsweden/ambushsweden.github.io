f49e8f13cb1b037efd56ab2250b6f5d891eb4d4a
RydenMartin <Martin@Ms-MacBook-Pro.local>
2016-04-01 12:25:16 +0200

    ändrat merch + media
    länk till tic tail och instagram feed under picture i media

--- js/webflow.js
+++ js/webflow.js
@@ -51,28 +51,30 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	__webpack_require__(4);
 	__webpack_require__(5);
 	__webpack_require__(6);
 	__webpack_require__(7);
 	__webpack_require__(8);
 	__webpack_require__(9);
-	__webpack_require__(1);
 	__webpack_require__(10);
 	__webpack_require__(11);
 	__webpack_require__(12);
+	__webpack_require__(1);
 	__webpack_require__(13);
 	__webpack_require__(14);
 	__webpack_require__(15);
 	__webpack_require__(16);
-	module.exports = __webpack_require__(17);
+	__webpack_require__(17);
+	__webpack_require__(18);
+	__webpack_require__(19);
+	module.exports = __webpack_require__(20);
 
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	/* WEBPACK VAR INJECTION */(function(process) {'use strict';
 
 	/**
 	 * Webflow: Core site library
@@ -86,12 +88,11 @@
 	var $win = $(window);
 	var $doc = $(document);
 	var isFunction = $.isFunction;
-	var _ = Webflow._ = __webpack_require__(18);
+	var _ = Webflow._ = __webpack_require__(21);
 	var tram = __webpack_require__(3) && $.tram;
 	var domready = false;
 	var destroyed = false;
 	var Modernizr = window.Modernizr;
-	var noop = function() {};
 	tram.config.hideBackface = false;
 	tram.config.keepInherited = true;
 
@@ -187,7 +188,7 @@
 	  if (mode === 'preview') return inApp && !designFlag;
 	  if (mode === 'slug') return inApp && window.__wf_slug;
 	  if (mode === 'editor') return window.WebflowEditor;
-	  if (mode === 'test') return window.__wf_test;
+	  if (mode === 'test') return process.env.NODE_ENV === 'test' || window.__wf_test;
 	  if (mode === 'frame') return window !== window.top;
 	};
 
@@ -370,6 +371,7 @@
 	// Export commonjs module
 	module.exports = window.Webflow = Webflow;
 
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4)))
 
 /***/ },
 /* 2 */
@@ -454,6 +456,166 @@
 
 /***/ },
 /* 4 */
+/***/ function(module, exports) {
+
+	// shim for using process in browser
+
+	var process = module.exports = {};
+	var queue = [];
+	var draining = false;
+	var currentQueue;
+	var queueIndex = -1;
+
+	function cleanUpNextTick() {
+	    draining = false;
+	    if (currentQueue.length) {
+	        queue = currentQueue.concat(queue);
+	    } else {
+	        queueIndex = -1;
+	    }
+	    if (queue.length) {
+	        drainQueue();
+	    }
+	}
+
+	function drainQueue() {
+	    if (draining) {
+	        return;
+	    }
+	    var timeout = setTimeout(cleanUpNextTick);
+	    draining = true;
+
+	    var len = queue.length;
+	    while(len) {
+	        currentQueue = queue;
+	        queue = [];
+	        while (++queueIndex < len) {
+	            if (currentQueue) {
+	                currentQueue[queueIndex].run();
+	            }
+	        }
+	        queueIndex = -1;
+	        len = queue.length;
+	    }
+	    currentQueue = null;
+	    draining = false;
+	    clearTimeout(timeout);
+	}
+
+	process.nextTick = function (fun) {
+	    var args = new Array(arguments.length - 1);
+	    if (arguments.length > 1) {
+	        for (var i = 1; i < arguments.length; i++) {
+	            args[i - 1] = arguments[i];
+	        }
+	    }
+	    queue.push(new Item(fun, args));
+	    if (queue.length === 1 && !draining) {
+	        setTimeout(drainQueue, 0);
+	    }
+	};
+
+	// v8 likes predictible objects
+	function Item(fun, array) {
+	    this.fun = fun;
+	    this.array = array;
+	}
+	Item.prototype.run = function () {
+	    this.fun.apply(null, this.array);
+	};
+	process.title = 'browser';
+	process.browser = true;
+	process.env = {};
+	process.argv = [];
+	process.version = ''; // empty string to avoid regexp issues
+	process.versions = {};
+
+	function noop() {}
+
+	process.on = noop;
+	process.addListener = noop;
+	process.once = noop;
+	process.off = noop;
+	process.removeListener = noop;
+	process.removeAllListeners = noop;
+	process.emit = noop;
+
+	process.binding = function (name) {
+	    throw new Error('process.binding is not supported');
+	};
+
+	process.cwd = function () { return '/' };
+	process.chdir = function (dir) {
+	    throw new Error('process.chdir is not supported');
+	};
+	process.umask = function() { return 0; };
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	/**
+	 * Webflow: Background Video component
+	 */
+
+	var Webflow = __webpack_require__(1);
+
+	Webflow.define('backgroundVideo', module.exports = function ($, _) {
+	  var namespace = '.w-background-video';
+	  var doc = $(document);
+
+	  function ready () {
+	    var backgroundVideoNodes = $(document).find('.w-background-video');
+	    if (backgroundVideoNodes.length === 0) {
+	      return;
+	    }
+
+	    backgroundVideoNodes.each(function (_, node) {
+	      $(node).prepend(createVideoNode(node));
+	    });
+	  }
+
+	  function createVideoNode (nativeNode) {
+	    var nodeData = nativeNode.dataset;
+
+	    if (!nodeData.videoUrls) {
+	      return $('<video />');
+	    }
+
+	    // Prevent loading the videos on mobile browsers as its likely that they
+	    // are on low-bandwidth connections.
+	    if (Webflow.isMobile()) {
+	      return $('<video />').css('background-image', 'url(' + nodeData.posterUrl + ')');
+	    }
+
+	    var videoURLs = nodeData.videoUrls.split(',');
+	    var sourceNodes = videoURLs.map(function (url) {
+	      return $('<source />').attr({
+	        src: url,
+	        'data-wf-ignore': '',
+	      });
+	    });
+
+	    var videoNode = $('<video />').attr({
+	      autoplay: nodeData.autoplay,
+	      loop: nodeData.loop,
+	    })
+	    .css('background-image', 'url(' + nodeData.posterUrl + ')');
+
+	    videoNode.append(sourceNodes);
+
+	    return videoNode;
+	  }
+
+	  return { ready: ready };
+	});
+
+
+/***/ },
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -533,7 +695,7 @@
 
 
 /***/ },
-/* 5 */
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -733,7 +895,7 @@
 
 
 /***/ },
-/* 6 */
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -839,7 +1001,26 @@
 
 
 /***/ },
-/* 7 */
+/* 9 */
+/***/ function(module, exports) {
+
+	/**
+	 * Returns a Boolean representing whether or not the client is a mobile browser.
+	 *
+	 * NOTE: Many thanks to detectmobilebrowsers.com for this user agent detection
+	 * regex, without which the mobile internet probably wouldn't exist.
+	 */
+	Webflow.isMobile = function () {
+	  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+	  return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i
+	         .test(userAgent) ||
+	         /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
+	         .test(userAgent.substr(0, 4));
+	}
+
+
+/***/ },
+/* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/**
@@ -852,7 +1033,7 @@
 	  var api = {};
 
 	  // Cross-Domain AJAX for IE8
-	  __webpack_require__(19);
+	  __webpack_require__(22);
 
 	  var $doc = $(document);
 	  var $forms;
@@ -979,19 +1160,25 @@
 
 	      if (typeof value === 'string') value = $.trim(value);
 	      result[name] = value;
-	      status = status || getStatus(field, name, value);
+	      status = status || getStatus(field, type, name, value);
 	    });
 
 	    return status;
 	  }
 
-	  function getStatus(field, name, value) {
+	  function getStatus(field, type, name, value) {
 	    var status = null;
-	    if (!field.attr('required')) return null;
-	    if (!value) status = 'Please fill out the required field: ' + name;
-	    else if (emailField.test(name) || emailField.test(field.attr('type'))) {
-	      if (!emailValue.test(value)) status = 'Please enter a valid email address for: ' + name;
+
+	    if (type === 'password') {
+	      status = 'Passwords cannot be submitted.';
+	    } else if (field.attr('required')) {
+	      if (!value) {
+	        status = 'Please fill out the required field: ' + name;
+	      } else if (emailField.test(name) || emailField.test(field.attr('type'))) {
+	        if (!emailValue.test(value)) status = 'Please enter a valid email address for: ' + name;
+	      }
 	    }
+
 	    return status;
 	  }
 
@@ -1004,7 +1191,8 @@
 	      name: form.attr('data-name') || form.attr('name') || 'Untitled Form',
 	      source: loc.href,
 	      test: Webflow.env(),
-	      fields: {}
+	      fields: {},
+	      dolphin: /pass[\s-_]?(word|code)|secret|login|credentials/i.test(form.html())
 	    };
 
 	    preventDefault(data);
@@ -1134,7 +1322,7 @@
 
 
 /***/ },
-/* 8 */
+/* 11 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -1170,7 +1358,7 @@
 
 
 /***/ },
-/* 9 */
+/* 12 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -1602,7 +1790,7 @@
 
 
 /***/ },
-/* 10 */
+/* 13 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2269,7 +2457,7 @@
 
 
 /***/ },
-/* 11 */
+/* 14 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2381,7 +2569,7 @@
 
 
 /***/ },
-/* 12 */
+/* 15 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2605,7 +2793,7 @@
 
 
 /***/ },
-/* 13 */
+/* 16 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2632,12 +2820,20 @@
 	  var menuOpen = 'w--nav-menu-open';
 	  var linkOpen = 'w--nav-link-open';
 	  var ix = IXEvents.triggers;
+	  var menuSibling = $();
 
 	  // -----------------------------------
 	  // Module methods
 
 	  api.ready = api.design = api.preview = init;
-	  api.destroy = removeListeners;
+
+	  api.destroy = function() {
+	    menuSibling = $();
+	    removeListeners();
+	    if ($navbars && $navbars.length) {
+	      $navbars.each(teardown);
+	    }
+	  };
 
 	  // -----------------------------------
 	  // Private methods
@@ -2703,6 +2899,14 @@
 	    resize(i, el);
 	  }
 
+	  function teardown(i, el) {
+	    var data = $.data(el, namespace);
+	    if (data) {
+	      removeOverlay(data);
+	      $.removeData(el, namespace);
+	    }
+	  }
+
 	  function removeOverlay(data) {
 	    if (!data.overlay) return;
 	    close(data, true);
@@ -2735,7 +2939,7 @@
 	    config.easing2 = data.el.attr('data-easing2') || 'ease';
 
 	    var duration = data.el.attr('data-duration');
-	    config.duration = duration != null ? +duration : 400;
+	    config.duration = duration != null ? Number(duration) : 400;
 
 	    config.docHeight = data.el.attr('data-doc-height');
 
@@ -2765,7 +2969,7 @@
 
 	  function toggle(data) {
 	    // Debounce toggle to wait for accurate open state
-	    return _.debounce(function(evt) {
+	    return _.debounce(function() {
 	      data.open ? close(data) : open(data);
 	    });
 	  }
@@ -2861,6 +3065,7 @@
 
 	    // Add menu to overlay
 	    if (data.overlay) {
+	      menuSibling = data.menu.prev();
 	      data.overlay.show().append(data.menu);
 	    }
 
@@ -2898,7 +3103,6 @@
 	    data.button.removeClass(buttonOpen);
 	    var config = data.config;
 	    if (config.animation === 'none' || !tram.support.transform) immediate = true;
-	    var animation = config.animation;
 	    ix.outro(0, data.el[0]);
 
 	    // Stop listening for tap outside events
@@ -2935,8 +3139,8 @@
 	      data.menu.removeClass(menuOpen);
 	      data.links.removeClass(linkOpen);
 	      if (data.overlay && data.overlay.children().length) {
-	        // Move menu back to parent
-	        data.menu.appendTo(data.parent);
+	        // Move menu back to parent at the original location
+	        menuSibling.length ? data.menu.insertAfter(menuSibling) : data.menu.prependTo(data.parent);
 	        data.overlay.attr('style', '').hide();
 	      }
 
@@ -2951,7 +3155,7 @@
 
 
 /***/ },
-/* 14 */
+/* 17 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3109,7 +3313,7 @@
 
 
 /***/ },
-/* 15 */
+/* 18 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3619,7 +3823,7 @@
 
 
 /***/ },
-/* 16 */
+/* 19 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3811,8 +4015,15 @@
 
 	    // Fade in the new target
 	    function intro() {
-	      // Clear previous active class + inline style
-	      $previous.removeClass(tabActive).removeAttr('style');
+	      // Clear previous active class + styles touched by tram
+	      // We cannot remove the whole inline style because it could be dynamically bound
+	      $previous.removeClass(tabActive).css({
+	        opacity: '',
+	        transition: '',
+	        transform: '',
+	        width: '',
+	        height: ''
+	      });
 
 	      // Add active class to new target
 	      $targets.addClass(tabActive).each(ix.intro);
@@ -3836,7 +4047,7 @@
 
 
 /***/ },
-/* 17 */
+/* 20 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3978,7 +4189,7 @@
 
 
 /***/ },
-/* 18 */
+/* 21 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -4338,7 +4549,7 @@
 
 
 /***/ },
-/* 19 */
+/* 22 */
 /***/ function(module, exports) {
 
 	/*!
